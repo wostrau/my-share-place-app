4c786c034383e487ab2683b57bd3b0c216901222
wostrau <a.ostrovtsov@gmail.com>
2023-10-09 17:53:30 +0200

    added work with mongodb for storing and fetching user locations

--- src/MyPlace.js
+++ src/MyPlace.js
@@ -10,10 +10,26 @@ class LoadedPlace {
 
 const url = new URL(location.href);
 const queryParams = url.searchParams;
-const coords = {
-    lat: parseFloat(queryParams.get('lat')),
-    lng: parseFloat(queryParams.get('lng'))
-};
-const address = queryParams.get('address');
 
-new LoadedPlace(coords, address);
+// const coords = {
+//     lat: parseFloat(queryParams.get('lat')),
+//     lng: parseFloat(queryParams.get('lng'))
+// };
+// const address = queryParams.get('address');
+
+const locId = queryParams.get('location');
+
+fetch(`http://localhost:3000/location/${locId}`)
+    .then(response => {
+        if (response.status === 404) {
+            throw new Error('Could not find location!');
+        }
+
+        return response.json();
+    })
+    .then(data => {
+        new LoadedPlace(data.coordinates, data.address);
+    })
+    .catch(err => {
+        alert(err.message);
+    });
